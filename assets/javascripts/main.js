235c6cdb7b704aa295b803e9b4e8209833b267e7
Hane Liu <hane0131@gmail.com>
2015-06-13 19:49:04 +0800

    Enroll scroll event for navbar

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,3 +1,15 @@
 window.onload = function () {
-    // body...
+    var headerHeight = document.getElementsByTagName("header")[0].offsetHeight;
+
+    document.addEventListener("scroll", function(){
+
+        if(window.scrollY > headerHeight)
+        {
+            document.querySelector(".nav-front-page ul").style.display = "none";
+        }
+        else
+        {
+            document.querySelector(".nav-front-page ul").style.display = "block";
+        }
+    })
 }
