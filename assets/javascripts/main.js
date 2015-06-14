9f3cdb95394ccd5cea08856e2fbc0eedb139559b
Hane Liu <hane0131@gmail.com>
2015-06-14 11:43:22 +0800

    Change link's transform when scroll to specific content area

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,5 +1,7 @@
 window.onload = function () {
     var headerHeight = document.getElementsByTagName("header")[0].offsetHeight;
+    var content = document.getElementsByClassName("site-content");
+    var link = document.querySelectorAll(".link span");
 
     document.addEventListener("scroll", function(){
 
@@ -10,6 +12,21 @@ window.onload = function () {
         else
         {
             document.querySelector(".nav-front-page ul").style.display = "block";
+            for(i=0 ; i < link.length ; i++)
+            {
+                link[i].style.transform = "";
+            }
         }
-    })
+
+        for(i=0 ; i < content.length && window.scrollY >= content[i].offsetTop ; i++)
+        {
+            link[i].style.transform = "rotateX(90deg)";
+            for(j=0; j < link.length && j!=i; j++)
+            {
+                link[j].style.transform = "";
+            }
+        }
+
+    });
+
 }
